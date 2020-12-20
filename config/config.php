fcbc14b968b728d1fce1cb41258ab478cd04b22d
candradwicahyo <52719874+candradwicahyo@users.noreply.github.com>
2020-12-20 10:54:43 +0700

    membuat file config.php

---
+++ config/config.php
@@ -0,0 +1,19 @@
+<?php
+
+// konfigurasi database
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'app_buku');
+
+// default url
+define('BASE_URL', 'http://localhost:8000/page/app_buku/');
+
+// ekstensi
+define('EXT', ['.html', '.css', '.js', '.php']);
+
+// genre
+define('GENRE', ['petualang', 'komedi', 'aksi', 'fiksi ilmiah', 'horror', 'fantasi']);
+
+// nama author
+define('AUTHOR', 'created_by_candradwicahyo_' . date('Y') . '_');
