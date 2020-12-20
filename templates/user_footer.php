9560c634127e950ecfcbce3c5d1fb3ae1853225e
candradwicahyo <52719874+candradwicahyo@users.noreply.github.com>
2020-12-20 10:52:23 +0700

    membuat file user_footer.php

---
+++ templates/user_footer.php
@@ -0,0 +1,22 @@
+
+  <div class="container mb-3">
+    <footer class="mt-5">
+      <div class="row">
+        <div class="col">
+          <span class="text-black-50 d-flex justify-content-center align-items-center">© candra dwi cahyo | <?= date('Y'); ?></span>
+        </div>
+      </div>
+    </footer>
+  </div>
+    
+  <!-- javascript -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.6.0/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/10.12.5/sweetalert2.min.js"></script>
+  <script src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js"></script>
+  <script src="https://cdn.datatables.net/1.10.23/js/dataTables.bootstrap4.min.js"></script>
+  <script src="<?= base_url('assets/'); ?>js/script.js"></script>
+</body>
+
+</html>
